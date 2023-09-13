feaea6981cb57f79822ce2fda13bb4151bd5b754
dewansh01 <dewansh.new@gmail.com>
2023-09-13 23:25:42 +0530

    added filter and search feature

--- public/js/similarity.js
+++ public/js/similarity.js
@@ -2,10 +2,12 @@ function similarity(uarr1, uarr2) {
     let score = 0;
     let arr1 = Object.values(uarr1);
     let arr2 = Object.values(uarr2);
-    for (let i = 1; i < 10; i++) {
-        score += Math.abs(arr1[i] - arr2[i]);
+    for (let i = 1; i < 20; i++) {
+        if (arr1[i] == arr2[i]) {
+            score++;
+        }
     }
-    return score;
+    return (score/20)*100;
 }
 
 function getSimilarity(user_data) {
@@ -15,15 +17,29 @@ function getSimilarity(user_data) {
     for (let i = 0; i < len; i++) {
         similarityScore[user_array[i].name] = [];
         for (let j = 0; j < len; j++) {
-            if(i!=j){
+            if (i != j) {
                 let score = similarity(user_array[i].data, user_array[j].data);
-            similarityScore[user_array[i].name].push({ user: user_array[j].name, score: score });
+                similarityScore[user_array[i].name].push({ user: user_array[j].name, score: score });
             }
         }
     }
     return similarityScore;
 }
 
-    module.exports = {
-        getSimilarity: getSimilarity
-      };
+function getSimilarityWithFilter(user_data, candidate) {
+    let allResponse = getSimilarity(user_data);
+    let response = allResponse[candidate];
+    return response;
+}
+
+function getSimilarityBySearch(user_data, search) {
+    return getSimilarityWithFilter(user_data, search);
+}
+
+
+
+module.exports = {
+    getSimilarity: getSimilarity,
+    getSimilarityWithFilter: getSimilarityWithFilter,
+    getSimilarityBySearch: getSimilarityBySearch
+};
--- server.js
+++ server.js
@@ -21,6 +21,18 @@ app.post('/', (req, res) => {
 });
 
 app.get('/allSurvey', (req, res) => res.send({status:"pending"}));
+
+
 app.get('/similarity',(req,res) => res.send(JSON.stringify(similarity.getSimilarity(user_data))));
+app.get('/similarity/search',(req,res) => { //url will be like this localhost:3000/similarity/search?search=John
+    var search = req.query.search;
+    var response = similarity.getSimilarityBySearch(user_data,search);
+    res.json(response);
+});
+app.get('/similarity/:candidate',(req,res) => {
+    var candidate = req.params.candidate;
+    var response = similarity.getSimilarityWithFilter(user_data,candidate);
+    res.json(response);
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
